471069d5c92b0b907210defd7bd28b26c5246132
刘志付 <zhifu@liuzhifudeMacBook-Pro.local>
2017-09-08 00:38:24 +0800

    启动脚本修改，需要传递参数，dev 开发文档，test 测试文档

--- start.sh
+++ start.sh
@@ -1,1 +1,16 @@
-browser-sync start --server --files "**/*.html" "**/*.js" "**/*.css"
+if [ $# -ne 1 ]
+then
+  echo "缺少文档类型参数：dev(开发) | test(测试)"
+  exit 3
+fi
+
+case "$1" in
+    dev)
+        cd "./dev_doc"
+        browser-sync start --server --files "**/*.html" "**/*.js" "**/*.css"
+        ;;
+    test)
+        cd "./test_doc"
+        browser-sync start --server --files "**/*.html" "**/*.js" "**/*.css"
+        ;;
+esac
